6d86bf35aa910cd398024a1952e832bea320a12d
dyoshikawa <dyoshikawa1993@gmail.com>
2018-09-24 23:07:10 +0900

    db設定

--- build.sbt
+++ build.sbt
@@ -15,7 +15,11 @@ libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
   "ch.qos.logback" % "logback-classic" % "1.2.3" % "runtime",
   "org.eclipse.jetty" % "jetty-webapp" % "9.4.9.v20180320" % "container",
-  "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
+  "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
+  "com.typesafe.slick" %% "slick" % "3.2.0",
+  "com.h2database" % "h2" % "1.4.196",
+  "com.mchange" % "c3p0" % "0.9.5.2",
+  "com.typesafe" % "config" % "1.3.1"
 )
 
 enablePlugins(SbtTwirl)
--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,19 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource
+import javax.servlet.ServletContext
 import net.dyosh.kakeibo._
 import org.scalatra._
-import javax.servlet.ServletContext
+import org.slf4j.LoggerFactory
+import slick.jdbc.H2Profile.api._
 
 class ScalatraBootstrap extends LifeCycle {
+
+  val logger = LoggerFactory.getLogger(getClass)
+
+  val cpds = new ComboPooledDataSource
+  logger.info("Created c3p0 connection pool")
+
   override def init(context: ServletContext) {
-    context.mount(new HouseholdAccountsServlet, "/*")
+    val db = Database.forDataSource(cpds, None)
+    context.mount(new HouseholdAccountsServlet(db), "/*")
   }
 }
